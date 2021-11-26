dd66a1a5a8c84fae598300f46889848fc77625ef
jean <jean18699@gmail.com>
2021-11-26 02:42:54 -0400

    Preparacion de vuejs para su uso

---
+++ frontend-aws-lambda/src/main.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter from 'vue-router';
+import App from "./App.vue";
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import listadoReservas from "./components/listadoReservas/listadoReservas.vue";
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
+
+const myRoutes = [
+  {path: "/listadoReservas", component: listadoReservas},
+]
+
+const router = new VueRouter({
+  routes: myRoutes
+})
+
+new Vue({
+  el: "#app",
+  router: router,
+  render: h => h(App)
+})
